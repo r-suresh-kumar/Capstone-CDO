b896f05d53d51956e58afe84ae2ad48d68aa5f01
Suresh <54197649+r-suresh-kumar@users.noreply.github.com>
2020-05-23 16:11:03 +0530

    Update build-docker-image.sh

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
-docker build --tag=nginx
-
-# Step 2: 
-# List docker images
-docker image ls
-
-# Step 3: 
-# Run flask app
-docker run -p 8000:80 nginx
+# Build docker image
+docker build -t $DOCKER_REPO:$TAG --no-cache .
