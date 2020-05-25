b12d5cdb8cb58c9ea4bd9cd716eb5b1675667a61
Suresh <54197649+r-suresh-kumar@users.noreply.github.com>
2020-05-25 16:27:55 +0530

    Update k8-deployment-logging.sh

--- scripts/k8-deployment-logging.sh
+++ scripts/k8-deployment-logging.sh
@@ -4,6 +4,8 @@
 # Display resource details post-deployment
 kubectl get services
 kubectl get pods -o wide
+kubectl describe deploy nginx
 kubectl get deployment nginx
+kubectl get deploy,rs,pod
 kubectl rollout status deployment nginx
 kubectl rollout history deployment nginx
